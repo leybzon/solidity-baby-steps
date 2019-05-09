88ae9a671ef4386211bb91e60ea2d4e7cc326e8d
Gene Leybzon <leybzon@gmail.com>
2019-05-09 15:16:04 -0700

    Added function to mint token for someone
    mintUniqueTokenTo()

--- contracts/75_ERC721.sol
+++ contracts/75_ERC721.sol
@@ -6,4 +6,13 @@ import 'openzeppelin-solidity/contracts/token/ERC721/ERC721Mintable.sol';
 contract MyNFT is ERC721Full, ERC721Mintable {
   constructor() ERC721Full("MyNFT1", "MNFT1") public {
   }
+  function mintUniqueTokenTo(
+    address _to,
+    uint256  _tokenId,
+    string  memory _tokenURI
+    ) public
+    {
+        super._mint(_to, _tokenId);
+        super._setTokenURI(_tokenId, _tokenURI);
+    }
 }
