0a5aeb3bafa919848d296a33b1901329d6196087
Gene Leybzon <leybzon@gmail.com>
2019-05-10 16:05:50 -0700

    Added auto-id for tokens

--- contracts/75_ERC721.sol
+++ contracts/75_ERC721.sol
@@ -6,13 +6,23 @@ import 'openzeppelin-solidity/contracts/token/ERC721/ERC721Mintable.sol';
 contract MyNFT is ERC721Full, ERC721Mintable {
   constructor() ERC721Full("MyNFT1", "MNFT1") public {
   }
-  function mintUniqueTokenTo(
-    address _to,
-    uint256  _tokenId,
-    string  memory _tokenURI
-    ) public
-    {
-        super._mint(_to, _tokenId);
-        super._setTokenURI(_tokenId, _tokenURI);
+ 
+    /**
+    * @dev Mints a token to an address with a tokenURI.
+    * @param _to address of the future owner of the token
+    * @param _tokenURI token URI for the token
+    */
+    function mintTo(address _to, string _tokenURI) public onlyOwner {
+        uint256 newTokenId = _getNextTokenId();
+        _mint(_to, newTokenId);
+        _setTokenURI(newTokenId, _tokenURI);
+    }
+
+    /**
+    * @dev calculates the next token ID based on totalSupply
+    * @return uint256 for the next token ID
+    */
+    function _getNextTokenId() private view returns (uint256) {
+        return totalSupply().add(1); 
     }
 }
