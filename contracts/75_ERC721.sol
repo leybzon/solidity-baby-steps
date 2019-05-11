e3b3d6f62e9bc0212f792207c0c20cd942dac2d1
Gene Leybzon <leybzon@gmail.com>
2019-05-11 04:53:15 -0700

    Update 75_ERC721.sol

--- contracts/75_ERC721.sol
+++ contracts/75_ERC721.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.5.0;
-import 'zeppelin-solidity/contracts/token/ERC721/ERC721Token.sol';
-import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
+import 'openzeppelin-solidity/contracts/token/ERC721/ERC721Token.sol';
+import 'openzeppelin-solidity/contracts/token/ERC721/Ownable.sol';
 
 contract GradientToken is ERC721Token, Ownable {
   string public constant name = "GradientToken";
