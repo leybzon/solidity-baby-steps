de253d3d45f64ab2a1446945cf0a955d31a2a480
Gene Leybzon <leybzon@gmail.com>
2019-05-11 04:38:35 -0700

    Update 75_ERC721.sol

--- contracts/75_ERC721.sol
+++ contracts/75_ERC721.sol
@@ -3,24 +3,27 @@ pragma solidity ^0.5.0;
 import 'openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol';
 import 'openzeppelin-solidity/contracts/token/ERC721/ERC721Mintable.sol';
 
-contract MyNFT is ERC721Full, ERC721Mintable {
-  constructor() ERC721Full("MyNFT1", "MNFT1") public {
-  }
- 
-    /**
-    * @dev Mints a token to an address with a tokenURI.
-    */
-    function mintOne() public onlyOwner {
-        uint256 newTokenId = _getNextTokenId();
-        _mint(msg.sender, newTokenId);
-        //_setTokenURI(newTokenId, _tokenURI);
-    }
+contract DoggoTime is ERC721Full {
+    using Counter for Counter.Index;
+    Counter.Index private tokenId;
 
-    /**
-    * @dev calculates the next token ID based on totalSupply
-    * @return uint256 for the next token ID
-    */
-    function _getNextTokenId() private view returns (uint256) {
-        return totalSupply().add(1); 
+    constructor(
+        string name,
+        string symbol,
+    )
+        ERC721Full(name, symbol)
+        public
+    {}
+
+    function createDoggoTimeframe(
+        string tokenURI
+    )
+        public
+        returns (bool)
+    {
+        uint256 doggoTokenId = tokenId.next();
+        _mint(msg.sender, doggoTokenId);
+        _setTokenURI(doggoTokenId, tokenURI);
+        return true;
     }
 }
