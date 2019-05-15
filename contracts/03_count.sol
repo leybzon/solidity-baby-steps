e793f721627cae841825ed636db3510778b2593b
Gene Leybzon <leybzon@gmail.com>
2019-05-15 15:53:05 -0700

    Added event

--- contracts/03_count.sol
+++ contracts/03_count.sol
@@ -3,8 +3,14 @@ pragma solidity ^0.5.0;
 contract Count {
   uint public value = 0;
   
+  event Changed(
+        uint _value
+  );
+  
   function plus() public returns (uint) {
      value++;
+     emit Changed(value);
+     
      return value;
   }
 }
