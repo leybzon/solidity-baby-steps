fbaac94c48ad6422eb884734dc44837641e50f9e
Gene Leybzon <leybzon@gmail.com>
2019-05-22 18:50:54 -0700

    Mint some tokens initially

--- contracts/90_ERC777.sol
+++ contracts/90_ERC777.sol
@@ -2,8 +2,9 @@ pragma solidity ^0.5.0;
 
 import "openzeppelin-solidity/contracts/token/ERC777/ERC777.sol";
 
+
 /**
- * @title ERC777 demo token
+ * @title ERC777
  */
 contract SimpleTokenERC777 is ERC777 {
     string public constant NAME="SimpleToken777";
@@ -11,10 +12,18 @@ contract SimpleTokenERC777 is ERC777 {
     uint8 public constant DECIMALS = 18;
     uint256 public constant INITIAL_SUPPLY = 10000 * (10 ** uint256(DECIMALS));
     address[]  operators;
+    
 
     /**
      * @dev Constructor adds msg.sender to the list of oerators
      */
     constructor () public ERC777(NAME,TOKEN,operators) {
+        address operator=msg.sender;
+        address to=msg.sender;
+        uint256 amount=INITIAL_SUPPLY;
+        bytes memory userData =  new bytes(1);
+        bytes memory operatorData =  new bytes(1);
+    
+       _mint(operator, to, amount, userData, operatorData);
     }
-}    
+}  
