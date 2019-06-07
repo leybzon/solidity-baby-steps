ba2ac0aad0f76600c67d552266cbfeac917cb29d
Gene Leybzon <leybzon@gmail.com>
2019-06-06 23:53:29 -0700

    Made pausable and burnable

--- contracts/76_ERC721.sol
+++ contracts/76_ERC721.sol
@@ -2,8 +2,10 @@ pragma solidity ^0.5.0;
 
 import 'openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol';
 import 'openzeppelin-solidity/contracts/token/ERC721/ERC721Mintable.sol';
+import 'openzeppelin-solidity/contracts/token/ERC721/ERC721Pausable.sol';
+import 'openzeppelin-solidity/contracts/token/ERC721/ERC721Burnable.sol';
 
-contract MyNFT is ERC721Full, ERC721Mintable {
+contract MyNFT is ERC721Full, ERC721Mintable, ERC721Pausable, ERC721Burnable  {
   constructor() ERC721Full("MyNFT1", "MNFT1") public {
   }
   
