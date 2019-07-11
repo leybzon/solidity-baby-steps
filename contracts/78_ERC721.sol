7e9c18c99b3c11d734fff11ead09e5dfd5bbc7e3
Gene Leybzon <leybzon@gmail.com>
2019-07-10 22:01:36 -0700

    Fixed path

--- contracts/78_ERC721.sol
+++ contracts/78_ERC721.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.5.0;
 
-import "./IERC721Receiver.sol";
+import "openzeppelin-solidity/contracts/token/ERC721/IERC721Receiver.sol";
 
 contract ERC721Holder is IERC721Receiver {
     function onERC721Received(address, address, uint256, bytes memory) public returns (bytes4) {
